0d96f37d533e2544895bb0abe0bf0bb40ea7e6de
M Zain <zainsultani333@gmail.com>
2026-07-08 03:29:04 +0500

    dfd

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://client-e-commerce-store-backend.vercel.app/api",
 });
 
 api.interceptors.request.use((config) => {
